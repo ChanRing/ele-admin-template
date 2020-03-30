fd299f95ae2fef6b0e94e9383835dc76c4377697
mango <miaozhihom@163.com>
2020-03-30 10:54:26 +0800

    feature：添加流程设计器的路由

---
+++ src/router/modules/flowGenerator.js
@@ -0,0 +1,16 @@
+/*
+ * @Description:流程设计器路由
+ * @Author: 志洪
+ * @Date: 2020-03-30 10:50:11
+ * @LastEditors: 志洪
+ * @LastEditTime: 2020-03-30 10:53:33
+ */
+export default [
+  {
+    path: '/flow',
+    name: 'flow',
+    meta: {
+      title: '流程设计器模块'
+    }
+  }
+]
