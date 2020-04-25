d13f0e7264336d947ac2518d0a626a6266a50cfd
changrong <laichangrong@live.cn>
2020-04-25 20:33:37 +0800

    webpack添加代码切割

--- vue.config.js
+++ vue.config.js
@@ -54,8 +54,34 @@ module.exports = {
         return options
       })
       .end()
+
+    // 代码切割
+    // config.optimization.splitChunks({
+    //   chunks: 'all',
+    //   cacheGroups: {
+    //     libs: {
+    //       name: 'chunk-libs',
+    //       test: /[\\/]node_modules[\\/]/,
+    //       priority: 10,
+    //       chunks: 'initial' // only package third parties that are initially dependent
+    //     },
+    //     vantUI: {
+    //       name: 'chunk-elementUI', // split vantUI into a single package
+    //       priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+    //       test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+    //     },
+    //     commons: {
+    //       name: 'chunk-commons',
+    //       test: resolve('src/components'), // can customize your rules
+    //       minChunks: 3, //  minimum common number
+    //       priority: 5,
+    //       reuseExistingChunk: true
+    //     }
+    //   }
+    // })
   },
   css: {
+    extract: false,
     loaderOptions: {
       scss: {
         // 传入共享的全局变量
