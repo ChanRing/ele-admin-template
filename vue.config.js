50a4ed518a515b84450332916d981d7f69d2a420
laich <laichangrong@live.cn>
2020-04-27 13:37:57 +0800

    去除模板无关代码

--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(
         new CompressionPlugin({
-          filename: '[path].gzip[query]', // 提示compression-webpack-plugin@2.0.0的话filename改为asset
+          filename: '[path].gzip[query]',
           algorithm: 'gzip',
           test: /\.js$|\.css$|\.html$/,
           threshold: 10240, //内容超过10KB进行压缩
@@ -55,30 +55,30 @@ module.exports = {
       })
       .end()
 
-    // 代码切割
-    // config.optimization.splitChunks({
-    //   chunks: 'all',
-    //   cacheGroups: {
-    //     libs: {
-    //       name: 'chunk-libs',
-    //       test: /[\\/]node_modules[\\/]/,
-    //       priority: 10,
-    //       chunks: 'initial' // only package third parties that are initially dependent
-    //     },
-    //     vantUI: {
-    //       name: 'chunk-elementUI', // split vantUI into a single package
-    //       priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-    //       test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-    //     },
-    //     commons: {
-    //       name: 'chunk-commons',
-    //       test: resolve('src/components'), // can customize your rules
-    //       minChunks: 3, //  minimum common number
-    //       priority: 5,
-    //       reuseExistingChunk: true
-    //     }
-    //   }
-    // })
+    // 代码切割 根据情况是否开启
+    config.optimization.splitChunks({
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: 'initial' // only package third parties that are initially dependent
+        },
+        elementUI: {
+          name: 'chunk-elementUI', // split elementUI into a single package
+          priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+          test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+        },
+        commons: {
+          name: 'chunk-commons',
+          test: resolve('src/components'), // can customize your rules
+          minChunks: 3, //  minimum common number
+          priority: 5,
+          reuseExistingChunk: true
+        }
+      }
+    })
   },
   css: {
     extract: false,
