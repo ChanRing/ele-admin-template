9b74767978ef3a3d6233f22f85e057ea032d78fd
laich <laichangrong@live.cn>
2020-04-27 13:44:08 +0800

    去除无关代码

--- mock/data/dataList1.js
+++ mock/data/dataList1.js
@@ -14,46 +14,6 @@ const tableData = Mock.mock({
   ]
 })
 
-// const menus = [
-//   {
-//     icon: 'el-icon-odometer',
-//     title: '处理中心',
-//     name: ''
-//   },
-//   {
-//     icon: 'el-icon-cpu',
-//     title: '我的工作台',
-//     name: '',
-//     children: [
-//       {
-//         icon: 'el-icon-news',
-//         title: '选项1',
-//         name: ''
-//       },
-//       {
-//         icon: 'el-icon-news',
-//         title: '选项2',
-//         name: ''
-//       },
-//       {
-//         icon: 'el-icon-news',
-//         title: '选项3',
-//         name: ''
-//       }
-//     ]
-//   },
-//   {
-//     icon: 'el-icon-message',
-//     title: '消息中心',
-//     name: ''
-//   },
-//   {
-//     icon: 'el-icon-notebook-2',
-//     title: '待办列表',
-//     name: ''
-//   }
-// ]
-
 export default [
   {
     url: '/table/get/v1',
